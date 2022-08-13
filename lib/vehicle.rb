1f7ea6356fc7833c8702863c63597468f516ec6e
Nick Warren <nickwarren47@gmail.com>
2022-08-13 10:22:31 -0600

    Done with assignment

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,25 @@
 class Vehicle
+    attr_accessor :wheel_size, :wheel_number
+  
+    def initialize(wheel_size, wheel_number)
+      @wheel_size = wheel_size
+      @wheel_number = wheel_number
+    end
+  
+    def go
+      "vrrrrrrrooom!"
+    end
+  
+    def fill_up_tank
+      "filling up!"
+    end
 
+    def check_oil 
+        "oil looks good"
+    end
+
+    def check_actuator
+        "sparks be a flyin from this thing"
+    end
 end
+
