29ddb582a83f8664c7f260154c7edf1f90d4c5b1
Nick Warren <nickwarren47@gmail.com>
2022-08-13 10:19:29 -0600

    commit tree 0acdc70c5aa671d9d37c81ad5fe5d0ece33947d4

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,13 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
+# require_relative 'lib/vehicle.rb'
+
+class Car < Vehicle
+    # let's overwrite the "#go" method on the vehicle.rb
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+end
+
+# car1 = Car.new(medium, 4)
+# car2 = Car.new(large, 8)
